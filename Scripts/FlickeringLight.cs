26408132a06d42fa88e2b31b3bfe674209a9cff6
Marios Michailidis <ma.michailidis@gmail.com>
2015-08-26 23:47:35 +0300

    Version 3.0

--- Scripts/FlickeringLight.cs
+++ Scripts/FlickeringLight.cs
@@ -1,5 +1,5 @@
 ﻿/* ----------------------------------------------
- * Version 2.0
+ * Version 3.0
  * Flickering Light * (C)2015 Michailidis Marios
  * 
  * ma.michailidis@gmail.com	
@@ -35,7 +35,7 @@ public class FlickeringLight : MonoBehaviour {
 	public LightSelection LightType = LightSelection.Flickering;
 	[Header("Light Speed Settings :", order=0)]
 	[Space(5, order=1)]
-	public float Speed;
+	public float Speed = 1.0F;
 	public float SpeedOffset;
 	[Range(0.0f, 1.0f)]
 	public float SpeedFluctuation;
@@ -48,13 +48,16 @@ public class FlickeringLight : MonoBehaviour {
 	// Internal Settings
 	Light myLight;
 	Transform myTransform;
-	bool xStart, xEnd;
+	bool xStart, xEnd, xPosReset;
 	float xPos, xMin, xMax;
-	bool yStart, yEnd;
+	bool yStart, yEnd, yPosReset;
 	float yPos, yMin, yMax;
-	bool zStart, zEnd;
+	bool zStart, zEnd, zPosReset;
 	float zPos, zMin, zMax;
-	
+	bool lightStart, lightEnd;
+	float lightMin, lightMax;
+	float currentSpeed;
+	bool isRunning;
 	float targetIntensity;
 	float timePassed;
 	float actionTime;
@@ -75,25 +78,43 @@ public class FlickeringLight : MonoBehaviour {
 		myTransform = GetComponent(typeof(Transform)) as Transform;
 
 		xPos = myTransform.position.x;
+		xPosReset = true;
 		xMin = XAxis.MinimumOffset;
 		xMax = XAxis.MaximumOffset;
 		yPos = myTransform.position.y;
+		yPosReset = true;
 		yMin = YAxis.MinimumOffset;
 		yMax = YAxis.MaximumOffset;
 		zPos = myTransform.position.z;
+		xPosReset = true;
 		zMin = ZAxis.MinimumOffset;
 		zMax = ZAxis.MaximumOffset;
+		currentSpeed = Speed / 10;
 	}
 
 	void FixedUpdate()
 	{
-		LightIntensity ();
+		switch (LightType) {
+		case LightSelection.Flickering:
+			Flickering ();
+			break;
+		case LightSelection.Fluorecent:
+			if (!isRunning) StartCoroutine(Fluorecent());
+			break;
+		}
+
 		LightMovement ();
 	}
 
 	void LightMovement()
 	{
 		if (XAxis.EnableMovement) {
+			if (xPosReset)
+			{
+				xPos = myTransform.position.x;
+				xPosReset = false;
+			}
+
 			if (xMax > XAxis.MaximumOffset)
 				xMax = XAxis.MaximumOffset;
 
@@ -140,151 +161,177 @@ public class FlickeringLight : MonoBehaviour {
 				myTransform.position.z
 				);
 		}
+		else
+		{
+			myTransform.position = new Vector3(xPos, myTransform.position.y, myTransform.position.z);
+			xPosReset = true;
+		}
 
 		if (YAxis.EnableMovement) {
+			if (yPosReset) {
+				yPos = myTransform.position.y;
+				yPosReset = false;
+			}
+
 			if (yMax > YAxis.MaximumOffset)
 				yMax = YAxis.MaximumOffset;
 			
 			if (yMin < YAxis.MinimumOffset)
 				yMin = YAxis.MinimumOffset;
 
-			if (transform.position.y >= yMax)
-			{
+			if (transform.position.y >= yMax) {
 				yEnd = true;
 
-				if (YAxis.Randomness > 0.0f && yStart)
-				{
-					if (Random.Range(0.0f, 1.1f) <= YAxis.Randomness)
-						yMax = Random.Range(yPos, YAxis.MaximumOffset);
-				}
-				else if (YAxis.Randomness == 0.0f)
+				if (YAxis.Randomness > 0.0f && yStart) {
+					if (Random.Range (0.0f, 1.1f) <= YAxis.Randomness)
+						yMax = Random.Range (yPos, YAxis.MaximumOffset);
+				} else if (YAxis.Randomness == 0.0f)
 					yMax = YAxis.MaximumOffset;
 				
 				yStart = false;
-			}
-			else if (transform.position.y <= yMin)
-			{
+			} else if (transform.position.y <= yMin) {
 				yEnd = false;
 
-				if (YAxis.Randomness > 0.0f && !yStart)
-				{
-					if (Random.Range(0.0f, 1.1f) <= YAxis.Randomness)
-						yMin = Random.Range(yPos, YAxis.MinimumOffset);
-				}
-				else if (YAxis.Randomness == 0.0f)
+				if (YAxis.Randomness > 0.0f && !yStart) {
+					if (Random.Range (0.0f, 1.1f) <= YAxis.Randomness)
+						yMin = Random.Range (yPos, YAxis.MinimumOffset);
+				} else if (YAxis.Randomness == 0.0f)
 					yMin = YAxis.MinimumOffset;
 				
 				yStart = true;
 			}
 			
 			if (myTransform.position.y < yMax && !yEnd)
-				myTransform.position = new Vector3(myTransform.position.x, transform.position.y + YAxis.Speed, myTransform.position.z);
+				myTransform.position = new Vector3 (myTransform.position.x, transform.position.y + YAxis.Speed, myTransform.position.z);
 			else
-				myTransform.position = new Vector3(transform.position.x, myTransform.position.y - YAxis.Speed, myTransform.position.z);
+				myTransform.position = new Vector3 (transform.position.x, myTransform.position.y - YAxis.Speed, myTransform.position.z);
 
-			myTransform.position = new Vector3(
+			myTransform.position = new Vector3 (
 				myTransform.position.x,
 				Mathf.Clamp (myTransform.position.y, YAxis.MinimumOffset, YAxis.MaximumOffset),
 				myTransform.position.z
-				);
+			);
+		} else {
+			myTransform.position = new Vector3(myTransform.position.x, yPos, myTransform.position.z);
+			yPosReset = true;
 		}
 
 		if (ZAxis.EnableMovement) {
+			if (zPosReset) {
+				zPos = myTransform.position.z;
+				zPosReset = false;
+			}
+
 			if (zMax > ZAxis.MaximumOffset)
 				zMax = ZAxis.MaximumOffset;
 			
 			if (zMin < ZAxis.MinimumOffset)
 				zMin = ZAxis.MinimumOffset;
 
-			if (transform.position.z >= zMax)
-			{
+			if (transform.position.z >= zMax) {
 				zEnd = true;
 
-				if (ZAxis.Randomness > 0.0f && zStart)
-				{
-					if (Random.Range(0.0f, 1.1f) <= ZAxis.Randomness)
-						zMax = Random.Range(zPos, ZAxis.MaximumOffset);
-				}
-				else if (ZAxis.Randomness == 0.0f)
+				if (ZAxis.Randomness > 0.0f && zStart) {
+					if (Random.Range (0.0f, 1.1f) <= ZAxis.Randomness)
+						zMax = Random.Range (zPos, ZAxis.MaximumOffset);
+				} else if (ZAxis.Randomness == 0.0f)
 					zMax = ZAxis.MaximumOffset;
 				
 				zStart = false;
-			}
-			else if (transform.position.z <= zMin)
-			{
+			} else if (transform.position.z <= zMin) {
 				zEnd = false;
 
-				if (ZAxis.Randomness > 0.0f && !zStart)
-				{
-					if (Random.Range(0.0f, 1.1f) <= ZAxis.Randomness)
-						zMin = Random.Range(zPos, ZAxis.MinimumOffset);
-				}
-				else if (ZAxis.Randomness == 0.0f)
+				if (ZAxis.Randomness > 0.0f && !zStart) {
+					if (Random.Range (0.0f, 1.1f) <= ZAxis.Randomness)
+						zMin = Random.Range (zPos, ZAxis.MinimumOffset);
+				} else if (ZAxis.Randomness == 0.0f)
 					zMin = ZAxis.MinimumOffset;
 
 				zStart = true;
 			}
 			
 			if (myTransform.position.z < zMax && !zEnd)
-				myTransform.position = new Vector3(transform.position.x, myTransform.position.y, myTransform.position.z + ZAxis.Speed);
+				myTransform.position = new Vector3 (transform.position.x, myTransform.position.y, myTransform.position.z + ZAxis.Speed);
 			else
-				myTransform.position = new Vector3(transform.position.x, myTransform.position.y, myTransform.position.z - ZAxis.Speed);
+				myTransform.position = new Vector3 (transform.position.x, myTransform.position.y, myTransform.position.z - ZAxis.Speed);
 
-			myTransform.position = new Vector3(
+			myTransform.position = new Vector3 (
 				myTransform.position.x,
 				myTransform.position.y,
 				Mathf.Clamp (myTransform.position.z, ZAxis.MinimumOffset, ZAxis.MaximumOffset)
-				);
+			);
+		} else {
+			myTransform.position = new Vector3 (myTransform.position.x, myTransform.position.y, zPos);
+			zPosReset = true;
 		}
 	}
 
-	void LightIntensity()
+	void Flickering()
 	{
-		timePassed += Time.deltaTime;
-		float intensity = myLight.intensity;
-		float currentSpeed = Speed;
+		if (Speed > 0) {
+			float intensity = myLight.intensity;
 
-		// Speed Settings
-		if (SpeedOffset > 0 && Random.Range (0.0f, 1.1f) < SpeedFluctuation)
-			currentSpeed = Random.Range (Speed, SpeedOffset);
+			if (lightMax > MaximumIntensity)
+				lightMax = MaximumIntensity;
 
-		// Flickering Light
-		if (LightType == LightSelection.Flickering) {
-			intensity = Mathf.Lerp (intensity, targetIntensity, timePassed * currentSpeed);
+			if (lightMin < MinimumIntensity)
+				lightMin = MinimumIntensity;
 
-			// Flickering Randomness
-			if (Mathf.Abs (intensity - targetIntensity) < FlickeringRandomness) {
-				targetIntensity = Random.Range (MinimumIntensity, MaximumIntensity);
-				timePassed = 0.0f;
+			if (intensity >= lightMax) {
+				lightEnd = true;
+
+				if (FlickeringRandomness > 0.0f && lightStart) {
+					if (Random.Range(0.0f, 1.1f) <= FlickeringRandomness)
+							lightMax = Random.Range((MinimumIntensity +  MaximumIntensity) / 2, MaximumIntensity);
+				}
+				else if (FlickeringRandomness == 0.0f)
+					lightMax = MaximumIntensity;
+
+				lightStart = false;
 			}
-
-			// Smooth Flickering
-			if (FlickeringRandomness == 0)
-				intensity = Mathf.Cos (timePassed * currentSpeed * Mathf.PI) * (MaximumIntensity - MinimumIntensity) / 2 + (MaximumIntensity + MinimumIntensity) / 2;
-		}
-		// Fluorecent Light
-		else {
-			if (Speed != 0 && timePassed > actionTime)
+			else if (intensity <= lightMin)
 			{
-				if (SpeedFluctuation == 0)
-				{
-					if (intensity == MaximumIntensity)
-						intensity = MinimumIntensity;
-					else
-						intensity = MaximumIntensity;
-				}
+				lightEnd = false;
+
+				if (SpeedOffset > 0 && Random.Range (0.0f, 1.1f) < SpeedFluctuation)
+					currentSpeed = Random.Range (Speed, SpeedOffset) / 10;
 				else
-				{
-					if (Random.Range(0.0f, 1.1f) < SpeedFluctuation)
-				 	   intensity = MinimumIntensity;
-					else
-						intensity = MaximumIntensity;
+					currentSpeed = Speed / 10;
+
+				if (FlickeringRandomness > 0.0f && !lightStart) {
+					if (Random.Range(0.0f, 1.1f) <= FlickeringRandomness)
+						lightMin = Random.Range(MinimumIntensity, (MinimumIntensity + MaximumIntensity) / 2);
 				}
+				else if (FlickeringRandomness == 0.0f)
+					lightMin = MinimumIntensity;
 
-				actionTime = timePassed + (currentSpeed / 20);
+				lightStart = true;
 			}
-		}
 
-		myLight.intensity = intensity;
+			if (intensity < lightMax && !lightEnd)
+				intensity += currentSpeed;
+			else
+				intensity -= currentSpeed;
+
+			myLight.intensity = intensity;
+			Mathf.Clamp(myLight.intensity, MinimumIntensity, MaximumIntensity);
+		}
+	}
+
+	IEnumerator Fluorecent()
+	{
+		isRunning = true;
+
+		if (myLight.intensity == MaximumIntensity) {
+			if (SpeedFluctuation == 0)
+				myLight.intensity = MinimumIntensity;
+			else if (Random.Range (0.0f, 1.1f) < SpeedFluctuation)
+				myLight.intensity = MinimumIntensity;
+		}
+		else
+			myLight.intensity = MaximumIntensity;
+
+		yield return new WaitForSeconds (0.5f / Mathf.Abs(Speed));
+		isRunning = false;
 	}
 }
